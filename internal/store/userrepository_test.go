f70c51a7907c901f0d11f69340912e084404f909
Elmira Aliyeva <elmira.ali.98@gmail.com>
2020-10-22 23:07:40 +0500

    data validation

--- internal/store/userrepository_test.go
+++ internal/store/userrepository_test.go
@@ -13,9 +13,7 @@ func TestUserRepository_Create(t *testing.T) {
 	s, tearDown := store.TestStore(t, databaseURL)
 	defer tearDown("users")
 
-	u, err := s.User().Create(&model.User{
-		Email: "user@example.org",
-	})
+	u, err := s.User().Create(model.TestUser(t))
 	assert.NoError(t, err)
 	assert.NotNil(t, u)
 }
@@ -28,9 +26,10 @@ func TestUserRepository_FindByEmail(t *testing.T) {
 	_, err := s.User().FindByEmail(email)
 	assert.Error(t, err)
 
-	u, err := s.User().Create(&model.User{
-		Email: "user@example.org",
-	})
+	u := model.TestUser(t)
+	u.Email = email
+
+	s.User().Create(u)
 
 	u, err = s.User().FindByEmail(email)
 	assert.NoError(t, err)
